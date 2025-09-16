0df5f344288df86f3ad5b5247da77752861ed965
Meirhan Syzdykov <syzdykovmr@gmail.com>
2025-09-16 21:47:31 +0500

    feat: Create IAudio(Product) interface

---
+++ Assigment2/FactoryMethod/IAudio.java
@@ -0,0 +1,13 @@
+interface IAudio {
+    public String getTitle();
+    public void setTitle();
+
+    public String getAuthor();
+    public void setAuthor();
+
+    public int getDurationSec();
+    public void setDurationSec();
+
+    public String getPubDate();
+    public void setPubDate();
+}

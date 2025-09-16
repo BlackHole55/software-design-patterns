6b486ba6acf8ced5f06ec8e8f9798791708a1e42
Meirhan Syzdykov <syzdykovmr@gmail.com>
2025-09-16 21:54:39 +0500

    feat: Create AudioBook concrete class

---
+++ Assigment2/FactoryMethod/AudioBook.java
@@ -0,0 +1,49 @@
+class AudioBook implements IAudio {
+    private String title;
+    private String author;
+    private int durationSec;
+    private String pubDate;
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    
+    public int getDurationSec() {
+        return this.durationSec;
+    }
+
+    public void setDurationSec(int duration) {
+        this.durationSec = duration;
+    }
+
+
+    public String getPubDate() {
+        return this.pubDate;
+    }
+
+    public void setPubDate(String pubDate) {
+        this.pubDate = pubDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Title: " + this.title + 
+                " Author: " + this.author + 
+                " Duration(seconds): " + this.durationSec + 
+                " Published Date: " + this.pubDate;
+    }
+}

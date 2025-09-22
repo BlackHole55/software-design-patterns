b44549f8470e735d661154474be4aadc115e1aa8
Meirhan Syzdykov <syzdykovmr@gmail.com>
2025-09-22 10:45:14 +0500

    refactor: rename vars in main

--- Assigment2/AbstractFactory/Main.java
+++ Assigment2/AbstractFactory/Main.java
@@ -10,9 +10,9 @@ class Main {
             "1985"
         );
 
-        Application app = new Application(musicFactory);
-        app.play();
-        app.info();
+        Application musicApp = new Application(musicFactory);
+        musicApp.play();
+        musicApp.info();
 
         IAudioFactory audiobookFactory = new AudioBookFactory(
             "Hitchhiker's Guide to the Galaxy", 
@@ -22,8 +22,8 @@ class Main {
             "12-10-1979"
         );
 
-        Application app2 = new Application(audiobookFactory);
-        app2.play();
-        app2.info();
+        Application audiobookApp2 = new Application(audiobookFactory);
+        audiobookApp2.play();
+        audiobookApp2.info();
     }
 }

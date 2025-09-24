c5509724599a876b21196b97bbfe23fd79c07d48
Meirhan Syzdykov <syzdykovmr@gmail.com>
2025-09-24 14:45:27 +0500

    feat: Create main

---
+++ Assignment3/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        MediaAdapter musicAdapter = new MediaAdapter(new MusicPlayer());
+        
+        musicAdapter.play("Tom's Diner - DNA");
+
+        MediaAdapter audiobookAdapter = new MediaAdapter(new AudiobookPlayer());
+
+        audiobookAdapter.play("Brave New World - Aldous Huxley");
+
+
+        MusicAdapter musicAdapter2 = new MusicAdapter();
+        musicAdapter2.play("Bird's Lament - Moondog");
+    }
+}

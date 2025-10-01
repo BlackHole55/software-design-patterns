4ea33cb0bdd45e94184bd5554feb3b3083266af4
Meirhan Syzdykov <syzdykovmr@gmail.com>
2025-10-01 18:34:54 +0500

    feat: Create client

---
+++ Assignment4/Main.java
@@ -0,0 +1,25 @@
+class Main {
+    public static void main(String[] args) {
+        Music wheresYourHeadAt = new Music(
+            "Where's Your Head At", 
+            "Basement Jaxx", 
+            "Rooty", 
+            "2001"
+        );
+
+        Audiobook littlePrince = new Audiobook(
+            "The Little Prince", 
+            "Antoine de Saint-Exupery", 
+            "Morgan Freeman", 
+            "1943"
+        );
+
+        BasicPlayer basicMusicPlayer = new BasicPlayer(wheresYourHeadAt);
+        basicMusicPlayer.play();
+        System.out.println(basicMusicPlayer.getInfo());
+
+        BasicPlayer basicAudiobookPlayer = new BasicPlayer(littlePrince);
+        basicAudiobookPlayer.play();
+        System.out.println(basicAudiobookPlayer.getInfo());
+    }    
+}

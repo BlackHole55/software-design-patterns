b6fa32d27008d394125b70168f392ce36e014d9b
Meirhan Syzdykov <syzdykovmr@gmail.com>
2025-11-01 12:30:34 +0500

    feat: Add StateKey class to store state keys

--- Assignment7/Main.java
+++ Assignment7/Main.java
@@ -1,15 +1,18 @@
 package Assignment7;
 
+import Assignment7.ConcretePublisher.SmartCamera;
 import Assignment7.ConcretePublisher.SmartHeater;
+import Assignment7.ConcreteSubscribers.SecurityDevice;
 import Assignment7.ConcreteSubscribers.SmartHomeUser;
+import Assignment7.Utils.StateKeys;
 
 public class Main {
     public static void main(String[] args) {
         SmartHeater heaterCelsius1 = new SmartHeater(); 
         
-        heaterCelsius1.setAttribute("temperature", "25C");
-        heaterCelsius1.setAttribute("humidity", "50%");
-        heaterCelsius1.setAttribute("heaterStatus", "On");
+        heaterCelsius1.setState(StateKeys.TEMPERATURE, "25C");
+        heaterCelsius1.setState(StateKeys.HUMIDITY, "50%");
+        heaterCelsius1.setState(StateKeys.HEATER_STATUS, "On");
 
         SmartHomeUser marvinUser = new SmartHomeUser("Marvin");
         SmartHomeUser arthurUser = new SmartHomeUser("Arthur");
@@ -20,12 +23,26 @@ public class Main {
         heaterCelsius1.subscribe(douglasUser);
         heaterCelsius1.notifySubscribers();
 
-        System.out.println("temperature: " + marvinUser.getHeaterAttribute("temperature"));
-        System.out.println("humidity: "+ arthurUser.getHeaterAttribute("humidity"));
-        System.out.println("heater status: " + douglasUser.getHeaterAttribute("heaterStatus"));
+        System.out.println(marvinUser.getName() + " { temperature: " + marvinUser.getState(StateKeys.TEMPERATURE) + " }");
+        System.out.println(arthurUser.getName() + " { humidity: "+ arthurUser.getState(StateKeys.HUMIDITY) + " }");
+        System.out.println(douglasUser.getName() + " { heater status: " + douglasUser.getState(StateKeys.HEATER_STATUS) + " }");
 
-        heaterCelsius1.setAttribute("heaterStatus", "Off");
+        heaterCelsius1.setState("heaterStatus", "Off");
 
-        System.out.println("heater status: " + marvinUser.getHeaterAttribute("heaterStatus"));
+        System.out.println(marvinUser.getName() + " { heater status: " + marvinUser.getState(StateKeys.HEATER_STATUS) + " }");
+
+        SmartCamera camera1 = new SmartCamera();
+
+        SecurityDevice secDevice1 = new SecurityDevice(1);
+
+        camera1.subscribe(secDevice1);
+        camera1.subscribe(arthurUser);
+        camera1.subscribe(douglasUser);
+
+        camera1.setState(StateKeys.CAMERA_STATUS, "On");
+        camera1.setState(StateKeys.MOTION_DETECTED, "True");
+
+        System.out.println(secDevice1.getId() + " { camera status: " + secDevice1.getState(StateKeys.CAMERA_STATUS) + " }");
+        System.out.println(arthurUser.getName() + " { mothion detected: " + arthurUser.getState(StateKeys.MOTION_DETECTED) + " }");
     }
 }
---
+++ Assignment7/Utils/StateKeys.java
@@ -0,0 +1,14 @@
+package Assignment7.Utils;
+
+public final class StateKeys {
+    private StateKeys() {}
+
+    // Heater
+    public static final String TEMPERATURE = "temperature";
+    public static final String HUMIDITY = "humidity";
+    public static final String HEATER_STATUS = "heaterStatus";
+
+    // Camera
+    public static final String MOTION_DETECTED = "motionDetected";
+    public static final String CAMERA_STATUS = "cameraStatus";
+}

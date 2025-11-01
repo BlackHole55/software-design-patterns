f742808f4452fa93030660cbd2d4ec95a804c4ef
Meirhan Syzdykov <syzdykovmr@gmail.com>
2025-11-01 12:28:59 +0500

    feat: Add SecurityDevice concrete subscriber

---
+++ Assignment7/ConcreteSubscribers/SecurityDevice.java
@@ -0,0 +1,29 @@
+package Assignment7.ConcreteSubscribers;
+
+import java.util.HashMap;
+
+import Assignment7.Subscriber.ISubscriber;
+
+public class SecurityDevice implements ISubscriber {
+    private final int ID; 
+    private HashMap<String, String> state = new HashMap<>();
+    private final String KEY_MISSING_MESSAGE = "No such key, please try another one";
+
+
+    public SecurityDevice(int ID) {
+        this.ID = ID;
+    }
+
+    @Override
+    public void update(HashMap<String, String> state) {
+        this.state = state;
+    }
+
+    public String getState(String key) {
+        return this.state.getOrDefault(key, KEY_MISSING_MESSAGE);
+    }
+
+    public int getId() {
+        return this.ID;
+    }
+}
